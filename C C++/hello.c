8a3447804465eb33771dfce8d6e41175341fcb50
augustinegwon <augustinegwon@gmail.com>
2022-04-02 12:52:18 +0900

    new

---
+++ C C++/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!");
+    printf("Godfather");
+
+    printf("2+5=%d\n", 2+91);
+    printf("2 %d %d %d %d +10=12341", 444 + 333, 333 +1, 444+1, 000-111111);
+
+    return 0;
+}
+
